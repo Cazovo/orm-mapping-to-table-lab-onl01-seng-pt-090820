d3fd4bea601ae1dedea826f5c9f77b75deafb386
conner fortner <fortner.conner@yahoo.com>
2020-11-05 21:32:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,43 +1,45 @@
 class Student
-  attr_accessor :id, :name, :grade
+     attr_accessor :name, :grade
+  attr_reader :id
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
 
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT,
+        grade INTEGER
+        )
+    SQL
+      DB[:conn].execute(sql)
   end
 
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)  
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
 
-    DB[:conn].execute(sql)
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
 end
